566cbcf9b6c7b65f4d4d73dd868d08306ebe8360
Derlys <derlysr6@gmail.com>
2022-11-19 06:20:15 -0300

    add kapt plugin

--- app/src/main/java/com/example/booksrecomended/data/api/BooksApi.kt
+++ app/src/main/java/com/example/booksrecomended/data/api/BooksApi.kt
@@ -2,9 +2,13 @@ package com.example.booksrecomended.data.api
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface BooksApi {
 
     @GET("list.json")
-    fun listRepos(): Call<List<>>?
+    fun listRepos(
+        @Query("api-key")apiKey: String = "rPB9mjmZ22iRT1LetWhrG1PmYTntaPs4",
+        @Query("list") list: String = "hardcover-fiction"
+    ): Call<List<>>?
 }
